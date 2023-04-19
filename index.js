4bd37824c6c1b88964207fec39406af519edc946
rubycates <123592566+rubycates@users.noreply.github.com>
2023-04-19 13:29:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.red').hover(function() {
+        $(this).css("transform", "scale(1.03)");
+    },
+    function() {
+        $(this).css("transform", "scale(1)");
+    });
+    $('.pink').hover(function() {
+        $(this).css("transform", "scale(1.03)");
+    },
+    function() {
+        $(this).css("transform", "scale(1)");
+    });
+    $('.yellow').hover(function() {
+        $(this).css("transform", "scale(1.03)");
+    },
+    function() {
+        $(this).css("transform", "scale(1)");
+    });
+
+});
